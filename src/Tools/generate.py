cf8c3980211710e80ec30d5777c854286cfaf93c
Yorik van Havre <yorik@uncreated.net>
2013-04-11 10:59:42 -0300

    py3: ported pyCXX

--- src/Tools/generate.py
+++ src/Tools/generate.py
@@ -16,7 +16,7 @@ Generate source code out of an model definition.
 Autor:
   (c) 2006 Juergen Riegel
   juergen.riegel@web.de
-	Licence: GPL
+    Licence: GPL
 
 Version:
   0.2
@@ -55,42 +55,44 @@ def generate(filename,path):
 
 
 def main():
-	defaultPath = ""
-	class generateOutput:
-		def write(self, data):
-			pass  
-	sys.stdout=generateOutput()
-	
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help","outputPath="])
-	except getopt.GetoptError:
-		# print help information and exit:
-		sys.stderr.write(Usage)
-		sys.exit(2)
+    defaultPath = ""
+    class generateOutput:
+        def write(self, data):
+            pass
+        def flush(self):
+            pass
+    sys.stdout=generateOutput()
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help","outputPath="])
+    except getopt.GetoptError:
+        # print help information and exit:
+        sys.stderr.write(Usage)
+        sys.exit(2)
 
-	# checking on the options
-	for o, a in opts:
-		if o in ("-h", "--help"):
-			sys.stderr.write(Usage)
-			sys.exit()
-		if o in ("-o", "--outputPath"):
-			defaultPath = a
+    # checking on the options
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            sys.stderr.write(Usage)
+            sys.exit()
+        if o in ("-o", "--outputPath"):
+            defaultPath = a
 
 
-	# runing through the files
-	if (len(args) ==0):
-		#sys.stderr.write(Usage)
-		generate("../Mod/PartDesign/PartDesign_Model.xml")                  
-	else:
-		for i in args:
-			filename = os.path.abspath(i)
-			if(defaultPath == ""):
-				head,tail = os.path.split(filename)
-				print (head,tail)
-				generate(filename,head)
-			else:
-				generate(filename,defaultPath)
+    # runing through the files
+    if (len(args) ==0):
+        #sys.stderr.write(Usage)
+        generate("../Mod/PartDesign/PartDesign_Model.xml")                  
+    else:
+        for i in args:
+            filename = os.path.abspath(i)
+            if(defaultPath == ""):
+                head,tail = os.path.split(filename)
+                print (head,tail)
+                generate(filename,head)
+            else:
+                generate(filename,defaultPath)
 
-	
+    
 if __name__ == "__main__":
-	main()
+    main()
